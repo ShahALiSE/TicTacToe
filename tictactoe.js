a076afecb837e15cd5279a80d3491d00b5406c99
ShahALiSE <untold352@gmail.com>
2018-02-07 17:10:06 +0600

    practice

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,14 @@
 //JS code goes here
+
+var turn = false;
+	function play(event){
+
+		/*console.log(event);*/
+		if(!event.target.innerText){
+			event.target.innerText = turn? "0": "X";
+			turn = !turn;
+		}
+
+		
+		
+	}
